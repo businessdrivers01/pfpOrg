56e385efcbb12c74c5fd67d9274fced74bed8d64
hafizasad419 <hafizasad419@gmail.com>
2024-09-26 18:58:11 +0500

    update benefits order

--- src/components/IFCE/IFCEMisc/Benefits.jsx
+++ src/components/IFCE/IFCEMisc/Benefits.jsx
@@ -7,6 +7,19 @@ import { MyButton } from '../../../utils'
 
 const benefitsData = [
     {
+        category: "For Exhibitors",
+        benefits: [
+            { text: "Showcase their products and expertise to visitors from industrial as well as trading sectors", icon: Truck },
+            { text: "Technical Guidance to improve the quality of their castings", icon: Wrench },
+            { text: "Get to know latest foundry products and technologies", icon: Microscope },
+            { text: "Understand casting requirements and needs to be presented by each industrial sector", icon: FileStack },
+            { text: "Localized development of imported castings", icon: MapPin },
+            { text: "Network with key stakeholders and industry leaders", icon: Users }
+
+        ]
+    },
+
+    {
         category: "For Visitors",
         benefits: [
             { text: "Meet Pakistan's foundry sector directly", icon: Users },
@@ -18,18 +31,6 @@ const benefitsData = [
 
         ]
     },
-    {
-        category: "For Exhibitors",
-        benefits: [
-            { text: "Showcase their products and expertise to visitors from industrial as well as trading sectors", icon: Truck },
-            { text: "Technical Guidance to improve the quality of their castings", icon: Wrench },
-            { text: "Get to know latest foundry products and technologies", icon: Microscope },
-            { text: "Understand casting requirements and needs to be presented by each industrial sector", icon: FileStack },
-            { text: "Localized development of imported castings", icon: MapPin },
-            { text: "Network with key stakeholders and industry leaders", icon: Users }
-
-        ]
-    }
 ]
 
 const BenefitCard = ({ text, Icon }) => (
@@ -44,7 +45,7 @@ function Benefits() {
         <div className="min-h-screen bg-lightGray py-12 px-4 sm:px-6 lg:px-8 font-sans">
             <div className="max-w-7xl mx-auto">
                 <h1 className="text-4xl font-bold text-center text-darkGray mb-12">Benefits of IFCE</h1>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                <div className="grid grid-cols-1 gap-8">
                     {benefitsData.map((category, index) => (
                         <div key={index} className="space-y-6">
                             <h2 className="text-2xl font-semibold text-orange mb-4">{category.category}</h2>
